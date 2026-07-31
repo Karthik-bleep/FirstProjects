62cc24051e04a93b2e118abbdc1c0efddc0a4eec
Karthik Swaminathan <karthikswaminathan123456@gmail.com>
2026-07-31 15:52:36 +0530

    Create Show Karthik in GUI.py

---
+++ Show Karthik in GUI.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow,QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+class MainWindow(QMainWindow):
+ def __init__(self):
+  super().__init__()
+  self.setWindowTitle("My Firsty GUI")
+  self.setWindowIcon(QIcon("cute.png"))
+  self.setGeometry(700,300,500,500)
+  label= QLabel("KARTHIK",self)
+  label.setFont(QFont("arial",30))
+  label.setGeometry(0,0,500,100)
+  label.setAlignment(Qt.AlignCenter)
+
+def main():
+ app =QApplication(sys.argv)
+ window=MainWindow()
+ window.show()
+ sys.exit(app.exec_())
+
+if __name__ == "__main__":
+ main()
